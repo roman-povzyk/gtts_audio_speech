10a1ff4c57b613fbb9218ce9840a252643476150
romanpovzyk <roman.povzyk@gmail.com>
2021-04-30 19:13:24 +0300

    Second commit

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from gtts import gTTS
 import os.path
+import datetime
 
 # The text file should be located in the project directory. Text encoding — ANSI.
 filename = "article.txt"
@@ -15,6 +16,7 @@ else:
 lang = None
 while (lang != "1") and (lang != "2") and (lang != "3"):
     lang = input("Choose the language (1/2/3 for ua/eng/ru): ")
+    date_start = datetime.datetime.now().second
     if lang == "1":
         print("Добре. Конвертую для тебе книгу українською. Це займе трошки часу.")
         obj = gTTS(text, lang="uk")
@@ -31,9 +33,10 @@ while (lang != "1") and (lang != "2") and (lang != "3"):
         print("You must choose one of three languages.")
 
 # Notification of readiness
+date_end = datetime.datetime.now().second
 if lang == "1":
-    print("Готово. Насолоджуйся читанням.")
+    print(f"Готово. Насолоджуйся читанням. {date_end - date_start} сек.")
 elif lang == "2":
-    print("Done. Enjoy your reading")
+    print(f"Done. Enjoy your reading. {date_end - date_start} sec.")
 elif lang == "3":
-    print("Есть. Наслаждайся чтением.")
+    print(f"Есть. Наслаждайся чтением. {date_end - date_start} сек.")
